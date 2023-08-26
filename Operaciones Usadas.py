55cf43aba8d65c7a48c736066196b82ceafc8c5f
Pacovs05 <141864140+Pacovs05@users.noreply.github.com>
2023-08-25 22:13:57 -0600

    Add files via upload
    Ejemplo de el uso de las operaciones que usaré en mi proyecto

---
+++ Operaciones Usadas.py
@@ -0,0 +1,12 @@
+n = int(450)
+precio = 0
+destino = input("¿A donde quieres viajar?: ")
+print(f"El destino seleccionado es: {destino}")
+Jalsico = 1
+if Jalsico :
+    #Precio + Seguro de viajero
+    precio = n*0.02 + n
+    print(f"El precio de su boleto será: ${precio} ")
+else : 
+    print("No tenemos viajes para su destino")
+#Solo utilicé Jalisco como prueba para representar como sería la suma de el seguro de viajero
