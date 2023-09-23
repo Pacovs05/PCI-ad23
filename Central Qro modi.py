fe9ccdd0f19d5e47a83ae979761138fc739c8ee0
Pacovs05 <141864140+Pacovs05@users.noreply.github.com>
2023-09-22 22:52:03 -0600

    Uso de ciclos

--- Central Qro modi.py
+++ Central Qro modi.py
@@ -10,7 +10,6 @@ def calcular_precio(ubicacion_elegida, precios):
         return precios[ubicacion_elegida] * 1.05
     else:
         return "Ubicación no válida"
-
 # Pedir al usuario que elija una ubicación
 print("Ubicaciones disponibles:")
 for i, ubicacion in enumerate(ubicaciones_disponibles):
@@ -18,5 +17,34 @@ for i, ubicacion in enumerate(ubicaciones_disponibles):
 
 opcion_ubicacion = int(input("Seleccione una ubicación (1-4): ")) - 1
 ubicacion_elegida = ubicaciones_disponibles[opcion_ubicacion]
+
+# Preguntar al usuario si quiere un viaje redondo o simple
+tipo_de_viaje = input("¿Quiere un viaje redondo o uno simple? (redondo/simple): ")
+
+# Validar la entrada del usuario
+while tipo_de_viaje.lower() not in ["redondo", "simple"]:
+    tipo_de_viaje = input("Por favor, ingrese 'redondo' o 'simple': ")
+
+# Preguntar al usuario el día para el viaje simple
+dia_viaje_simple = input("¿Qué día quiere elegir para el viaje de ida? ")
+
+# Preguntar al usuario el día de regreso para el viaje redondo si es necesario
+dia_viaje_regreso = None
+if tipo_de_viaje.lower() == "redondo":
+    dia_viaje_regreso = input("¿Qué día quiere elegir para el viaje de regreso? ")
+
+# Mostrar la información seleccionada por el usuario
+print("\nResumen de su elección:")
+print(f"Tipo de viaje: {tipo_de_viaje}")
+print(f"Día del viaje simple: {dia_viaje_simple}")
+if dia_viaje_regreso:
+    print(f"Día del viaje de regreso: {dia_viaje_regreso}")
+
 # Dar el precio
 print(f"El precio para ir a ", ubicacion_elegida,f"es : {calcular_precio(ubicacion_elegida, precios)}")
+
+if dia_viaje_regreso:
+    precio_total = calcular_precio(ubicacion_elegida, precios)
+    if tipo_de_viaje.lower() == "redondo":
+        precio_total *= 2
+    print(f"El precio total es: {precio_total}")
